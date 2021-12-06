61053f7bb395f35f7c16187c321c14666d27e0c6
LCzunko <LCzunko@gmail.com>
2021-12-06 14:24:04 +0100

    Add error-handling filter

---
+++ Controllers/ErrorHandler/ErrorHandlingFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace EduResourceAPI.Controllers.ErrorHandler
+{
+    public class ErrorHandlingFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var exception = context.Exception;
+            ILogger logger = LoggerFactory.Create(x => x.AddConsole()).CreateLogger("ErrorHandlingFilter");
+            logger.LogCritical($"Exception message: {exception.Message}\nStack trace:\n{exception.StackTrace}");
+
+            context.HttpContext.Response.StatusCode = 500;
+            context.ExceptionHandled = true; //optional 
+        }
+    }
+}
