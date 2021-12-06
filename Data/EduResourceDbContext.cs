7aabaf2b2b5366f5982778243bc700453e12840e
LCzunko <LCzunko@gmail.com>
2021-12-06 14:22:46 +0100

    Add database context

---
+++ Data/EduResourceDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EduResourceAPI.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduResourceAPI.Data
+{
+    public class EduResourceDbContext : IdentityDbContext
+    {
+        public DbSet<Material> Materials { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Author> Authors { get; set; } = null!;
+        public DbSet<Review> Reviews { get; set; } = null!;
+
+        public EduResourceDbContext(DbContextOptions<EduResourceDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
