f8d842ecb20a496745dd33a0216ae958bbb37ba1
LCzunko <LCzunko@gmail.com>
2021-12-07 13:44:24 +0100

    Ignore null filter

--- Data/Repositories/IBaseRepository.cs
+++ Data/Repositories/IBaseRepository.cs
@@ -5,7 +5,7 @@ namespace EduResourceAPI.Data.Repositories
     public interface IBaseRepository<TEntity> where TEntity : class
     {
         Task Delete(int entityId);
-        Task<IList<TEntity>> Get(Expression<Func<TEntity, bool>> filter = null, string includeProperties = "");
+        Task<IList<TEntity>> Get(Expression<Func<TEntity, bool>> filter = null!, string includeProperties = "");
         void Insert(TEntity entity);
         void Update(TEntity entityToUpdate);
     }
